aebc51be694811d9682c7f4c90af2ef1b3156399
adityasetiawan34rpl-blip <aditya_setiawan_34rpl@student.smktelkom-mlg.sch.id>
2025-11-04 14:28:03 +0700

    Add files via upload

---
+++ Matrix.java
@@ -0,0 +1,48 @@
+import java.util.Scanner;
+
+public class Matrix {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah baris matriks: ");
+        int baris = input.nextInt();
+        System.out.print("Masukkan jumlah kolom matriks: ");
+        int kolom = input.nextInt();
+
+        int[][] matriks1 = new int[baris][kolom];
+        int[][] matriks2 = new int[baris][kolom];
+        int[][] hasil = new int[baris][kolom];
+
+        System.out.println("\nMasukkan elemen matriks pertama:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.printf("Elemen [%d][%d]: ", i+1, j+1);
+                matriks1[i][j] = input.nextInt();
+            }
+        }
+
+        System.out.println("\nMasukkan elemen matriks kedua:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.printf("Elemen [%d][%d]: ", i+1, j+1);
+                matriks2[i][j] = input.nextInt();
+            }
+        }
+
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                hasil[i][j] = matriks1[i][j] + matriks2[i][j];
+            }
+        }
+        
+        System.out.println("\nHasil penjumlahan matriks:");
+        for (int i = 0; i < baris; i++) {
+            for (int j = 0; j < kolom; j++) {
+                System.out.print(hasil[i][j] + " ");
+            }
+            System.out.println();
+        }
+        
+
+    }
+}
